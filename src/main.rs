9d62bb2de702cbb4af5790434a8012aeed0dad70
Freyja Valkyrie <fjvalkyrie@gmail.com>
2020-07-05 22:24:54 -0500

    Add dependencies.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,49 @@
-#![deny(missing_docs, clippy::all)] 
+#![deny(missing_docs, clippy::all)]
 //! Minimalist speedrun timer
 
-use conrod::{widget, color, Colorable, Positionable, Widget};
-use conrod::backend::glium::glium;
-use conrod::backend::glium::glium::{DisplayBuild, Surface};
 
-#[derive(Debug)]
-struct ApplicationMetaInfo {
-    name: String,
-    version : String,
-}
+use {
+    conrod::{
+        backend::glium::{
+            glium,
+            glium::{DisplayBuild, Surface},
+        },
+        color, widget, Colorable, Positionable, Widget,
+    },
+    clap::{App, Arg, crate_version, crate_authors},
+    toml::{Deserializer, Serializer},
+};
+
 
 fn main() {
-    // Setup meta information for build
-    let _meta = ApplicationMetaInfo { 
-        name: "Picosplit".to_string(), 
-        version : "0.0.1".to_string(),
-    };
+    // Process config
+
+    // TODO parse command line args
+    let args = App::new("Picosplit")
+       .version(crate_version!())
+       .about("Minimalist speedrun timer")
+       .author(crate_authors!())
+       .arg(Arg::with_name("cfg_dir")
+            .short("cfg")
+            .long("cfg-dir")
+            .value_name("FILE")
+            .default_value("./res/cfg/cfg.toml")
+            .help("Sets a path to load custom configuration file from")
+            .takes_value(true))
+       .get_matches(); 
+
+    // Build application window window.
+    let display = glium::glutin::WindowBuilder::new()
+        //.with_vsync()
+        .with_dimensions(400, 600)
+        .with_title("PicoSplit")
+        //.with_multisampling(4)
+        .build_glium()
+        .unwrap();
+    // Construct UI
+    // FIXME load from cfg
+    let mut ui =
+        conrod::UiBuilder::new([400.0, 600.0]).build();
 
     println!("Hello, world!");
 }
