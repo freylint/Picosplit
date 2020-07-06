9e503b6092bffa0a48b2a7d90ca66fcda907e1e2
Freyja Valkyrie <fjvalkyrie@gmail.com>
2020-07-05 21:23:19 -0500

    Setup basic project.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#![deny(missing_docs, clippy::all)] 
+//! Minimalist speedrun timer
+
+use conrod::{widget, color, Colorable, Positionable, Widget};
+use conrod::backend::glium::glium;
+use conrod::backend::glium::glium::{DisplayBuild, Surface};
+
+#[derive(Debug)]
+struct ApplicationMetaInfo {
+    name: String,
+    version : String,
+}
+
 fn main() {
+    // Setup meta information for build
+    let _meta = ApplicationMetaInfo { 
+        name: "Picosplit".to_string(), 
+        version : "0.0.1".to_string(),
+    };
+
     println!("Hello, world!");
 }
