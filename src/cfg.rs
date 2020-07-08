ca57715633532b54675deae4d7d4d065a3cdb18a
Freya Valkyrie <fjvalkyrie@gmail.com>
2020-07-07 22:32:57 -0500

    Create module to ease dealing with cfg files.

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,18 +1,21 @@
 //! Module containing logic for application configuration
 
+use {
+    serde::{Deserialize, Serialize},
+    std::{fs::File, io::prelude::*},
+};
+
 /// Struct containing application configuration data
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Cfg {
     title: String,
     width: u32,
-    height: u32
+    height: u32,
 }
 
-
 impl Cfg {
-
     /// Create Cfg from given data
-    pub fn new(title: String, width: u32, height: u32) -> Self{
+    pub fn new(title: String, width: u32, height: u32) -> Self {
         Cfg {
             title: title,
             width: width,
@@ -29,5 +32,28 @@ impl Cfg {
         }
     }
 
+    /// Read Cfg config from file
+    pub fn read(path: &str) -> Self {
+        // Open file
+        let mut file = File::open(path).unwrap();
+        let mut buffer = vec![];
 
+        // Read file to buffer
+        file.read(&mut buffer).unwrap();
+        
+        // Deserialize vuffer
+        toml::from_slice(&mut buffer).unwrap()
+    }
+
+    /// Writes a config to file
+    pub fn write(path: &str, cfg: &Cfg) -> std::io::Result<()> {
+        // Open file
+        let mut file = File::create(path)?;
+
+        // Buffer serialized default config
+        let buffer = toml::to_vec(cfg).unwrap();
+
+        // Write buffer to file
+        file.write_all(&buffer)
+    }
 }
