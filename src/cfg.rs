fc7b2da858e4fb208358b58fbbc25cbdb9913105
Freya Valkyrie <fjvalkyrie@gmail.com>
2020-07-08 01:11:52 -0500

    refactor config module

--- src/cfg.rs
+++ src/cfg.rs
@@ -2,51 +2,57 @@
 
 use {
     serde::{Deserialize, Serialize},
-    std::{fs::File, io::prelude::*},
+    std::{path::Path,fs::File, io::prelude::*},
 };
 
 /// Struct containing application configuration data
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Cfg {
-    title: String,
+    title: &'static str,
     width: u32,
     height: u32,
 }
 
 impl Cfg {
     /// Create Cfg from given data
-    pub fn new(title: String, width: u32, height: u32) -> Self {
+    pub fn new(title: &'static str, width: u32, height: u32) -> Self {
         Cfg {
-            title: title,
-            width: width,
-            height: height,
+            title,
+            width,
+            height,
         }
     }
 
     /// Create Cfg using default values
     pub fn default() -> Self {
-        Cfg {
-            title: "PicoSplit".to_owned(),
-            width: 400,
-            height: 600,
+        Self::new("PicoSplit", 400, 600)
+    }
+
+    pub fn init_cfg(cfg_path: &Path) -> Self {
+        // Check to see if config exists
+        if cfg_path.exists() {
+            // Load config
+            Self::read(cfg_path)
+        } else {
+            // Create default config
+            Self::write(cfg_path, &Self::default());
+
+
+            Self::read(cfg_path)
         }
+
     }
 
     /// Read Cfg config from file
-    pub fn read(path: &str) -> Self {
-        // Open file
-        let mut file = File::open(path).unwrap();
-        let mut buffer = vec![];
-
-        // Read file to buffer
-        file.read(&mut buffer).unwrap();
-        
-        // Deserialize vuffer
-        toml::from_slice(&mut buffer).unwrap()
+    pub fn read(path: &Path) -> Self {
+        let config = File::open(path);
+        let mut buf = "";
+        config.unwrap().read_to_string(&mut buf.to_string());
+        toml::from_str(buf).unwrap()
     }
 
     /// Writes a config to file
-    pub fn write(path: &str, cfg: &Cfg) -> std::io::Result<()> {
+    pub fn write(path: &Path, cfg: &Cfg) -> std::io::Result<()> {
         // Open file
         let mut file = File::create(path)?;
 
